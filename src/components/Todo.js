b63576a1a7fc2b4f2c32c6182b0cb4242af5b828
unknown <kanishka.m@Kanishka-IT.arpiconet.com>
2020-10-09 14:24:22 +0530

    Merge branch 'master' of https://github.com/kanishka-dot/simpleTodo_react

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,7 +6,11 @@ function Todo({todo}) {
             <input type="checkbox"/>
             <li
             style={{
+<<<<<<< HEAD
                 color:"white",
+=======
+                color="white",
+>>>>>>> f3b21471d50eba52705750151fdad3ee590e74db
                 textDecoration:todo.completed ? "line-through":null
             }}>{todo.task}</li>
             <button>X</button>
